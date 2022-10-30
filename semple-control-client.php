5448d38ecce94fbfbe8f2280d3d4f165f6195bfe
Alexander Merz <alexander.merz@gmail.com>
2022-10-30 23:57:21 +0100

    Squashed 'plugins/semple-control-client/' changes from 6016b00..fb19fcc
    fb19fcc namespace
    
    git-subtree-dir: plugins/semple-control-client
    git-subtree-split: fb19fcc7b4596b0578f056c16d8910399d861792

--- semple-control-client.php
+++ semple-control-client.php
@@ -14,7 +14,7 @@ Version: 0.0.1
 namespace SempleControlClient;
 
 // We need to track which plugin we are no matter of naming
-$plugin_data = get_plugin_data( __FILE__ );
+$plugin_data = \get_plugin_data( __FILE__ );
 $semple_control_plugin_name = $plugin_data['Name'];
 
 require_once __DIR__ . '/includes/Plugins.php';
