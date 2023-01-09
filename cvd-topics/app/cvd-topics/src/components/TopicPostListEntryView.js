9970a613c14e83aec3de4534a55889a68d825e36
alexmerz <alexander.merz@gmail.com>
2023-01-09 17:06:12 +0100

    add Date to List entries

--- cvd-topics/app/cvd-topics/src/components/TopicPostListEntryView.js
+++ cvd-topics/app/cvd-topics/src/components/TopicPostListEntryView.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ListGroup from 'react-bootstrap/ListGroup';
 import { useDrag } from "react-dnd";
+import {DateTime} from 'luxon';
 
 function TopicPostListEntryView(props) {
     const post = props.post;
@@ -16,11 +17,21 @@ function TopicPostListEntryView(props) {
         }
     }));
 
+    const dt = DateTime.fromISO(post.date_gmt);
+    let format = "dd.MM.yyyy, HH:mm";
+    let prefix = "geänd.: ";
+    if(dt.hasSame(new Date(), 'day')) {
+        format = "HH:mm";
+        prefix = "geänd.: heute, ";
+    }
+    const modified = prefix + dt.toFormat(format) + " Uhr";
+
     return (
         <ListGroup.Item ref={drag}>
             <div className="cvd-topics-listitem-title" dangerouslySetInnerHTML={{__html: post.title.rendered}} />
+            <div className="cvd-topics-listitem-modified" style={{ "font-size":"x-small" }}>({modified})</div>
             <div className="cvd-topics-listitem-menu"><a href={post.link} target="_blank" rel="noreferrer">Ansicht</a>|<a href={edit} target="_blank" rel="noreferrer">Bearbeiten</a></div>
-            </ListGroup.Item>
+        </ListGroup.Item>
         );
 }
 
